558e4b244941500c82e887b8f40854b8aa01bd4a
unknown <ayush02av@gmail.com>
2022-03-12 17:21:10 +0530

    Changes for deployment on heroku

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import os
+import django_heroku
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -159,3 +160,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+django_heroku.settings(locals())
